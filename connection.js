b6c956ac9059f8243953357d0c11a35f5b27f449
Abhinash Gupta <avinashguptaa0.3@gmail.com>
2025-07-18 07:31:44 +0545

    mongo problrm solve

--- connection.js
+++ connection.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
 function connectMongoDB(url) {
+  console.log("hello");
   mongoose
     .connect(url)
     .then(() => {
       console.log("MongoDB COnnected Sucessfully !");
     })
     .catch((error) => {
-      console.log("MongoDb Cannot Connected !");
+      console.log("MongoDb Cannot Connected !", error);
     });
 }
 
